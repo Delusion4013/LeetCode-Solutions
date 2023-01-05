1e1060dddc3ffdb29ef4400cb40ca0d89cda7f79
Delusion4013 <594431233@qq.com>
2023-01-05 22:49:03 +0800

    Daily Update

---
+++ 940.不同的子序列-ii.java
@@ -0,0 +1,26 @@
+import java.util.HashSet;
+import java.util.Set;
+
+/*
+ * @lc app=leetcode.cn id=940 lang=java
+ *
+ * [940] 不同的子序列 II
+ */
+
+// @lc code=start
+class Solution {
+    public int distinctSubseqII(String s) {
+        int MOD = 1000000007;
+        int ans = 0;
+        Set<String> set = new HashSet<String>();
+
+        
+        for(int i = 0; i < s.length(); i++) {
+            
+        }
+
+        return ans;
+    }
+}
+// @lc code=end
+
